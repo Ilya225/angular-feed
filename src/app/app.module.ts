6b4b29c6cdc018d89bc6f6cca8bc71b2c651b0d3
ilya225 <roilylitter@gmail.com>
2017-08-16 09:06:17 +0300

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { PostService } from './post/post.service';
+import { PostComponent } from './post/post.component';
 
 import { AppComponent } from './app.component';
 
+import { InMemoryDataService }  from './in-memory-data.service';
+
+import { AppRoutingModule } from './app-routing.module';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpModule
   ],
-  providers: [],
+  providers: [PostService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

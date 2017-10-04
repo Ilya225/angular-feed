07ea112ef5c2559258e56b5d6838105ba28cfcc2
ilya <ilyabarbashov09@gmail.com>
2017-10-04 17:50:55 +0300

    tmp

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,7 @@
  * API Domain config
  */
 export const API_HOST = "http://localhost";
-export const API_PORT = "8000";
+export const API_PORT = "9000";
 export const API_BASE_URL = `${API_HOST}:${API_PORT}`;
 
 

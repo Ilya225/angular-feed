c997a24bd5b29e3ab3eebba66b1e51dfeb13a44f
ilya225 <roilylitter@gmail.com>
2017-10-27 09:09:14 +0300

    tmp

--- src/app/login/login.service.ts
+++ src/app/login/login.service.ts
@@ -10,12 +10,17 @@ import 'rxjs/add/operator/toPromise';
 @Injectable()
 export class LoginService {
 
+    private headers = new Headers({'Content-Type': 'application/json'});
+
     constructor(
         private http: Http
     ) {}
 
     login(loginForm: LoginForm) :Promise<any> {
-        return this.http.post(`${API_BASE_URL}/login`, JSON.stringify(loginForm))
+        return this.http.post(
+            `${API_BASE_URL}/login`, 
+            JSON.stringify(loginForm), 
+            { headers: this.headers })
             .toPromise()
             .then(data => console.log(data))
             .catch(err => console.log(err));

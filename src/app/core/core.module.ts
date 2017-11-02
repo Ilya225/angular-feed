2b41c66b680a92eb7a12b4721635f314fdf92e93
ilya <ilyabarbashov09@gmail.com>
2017-11-02 17:12:58 +0200

    lazy module loading added

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,22 +2,29 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { CoreRoutingModule } from './core-routing.module';
 
+import { LoginService } from './login/login.service';
+import { LoginComponent } from './login/login.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
     imports: [
         CommonModule,
-        HttpModule
+        HttpModule,
+        FormsModule,
+        CoreRoutingModule
     ],
     exports: [
-        SidebarComponent
+        SidebarComponent,
+        LoginComponent
     ],
     providers: [
-
+        LoginService
     ],
     declarations: [
-        SidebarComponent
+        SidebarComponent,
+        LoginComponent
     ]
 })
 export class CoreModule {}
